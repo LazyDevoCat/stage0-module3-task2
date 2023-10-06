6d95cd53780410ef4e476daf984a0dfaf1e09d23
Maryia Maksimava <maryia_maksimava@epam.com>
2023-10-06 16:59:43 +0400

    first

--- src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
@@ -1,16 +1,16 @@
 package school.mjc.stage0.module3.task2;
 
-public class advancedNamingConvention {
+public class AdvancedNamingConvention {
     private static final int adult_age = 18;
 
     private int AGE;
     private int phoneNumber;
 
     void CAllToFriend() {
-        //some code
+        System.out.println("Hello");
     }
 
     void callByNumber(int Number) {
-        //some code
+        System.out.println(adult_age);
     }
 }
